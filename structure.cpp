3671af2febf191db8d9c8b5f53b806ec8043ee84
dasemenin <47276659+dasemenin@users.noreply.github.com>
2019-04-14 19:58:39 +0300

    Add files via upload

--- structure.cpp
+++ structure.cpp
@@ -23,22 +23,22 @@ struct Point
 /*
 int main()
 {
-	Point num = { 5,15 };
-	Point numer;
-	std::cin >> numer.x >> numer.y;
-	std::cout << numer.x << " " << numer.y << std::endl;
-	int R;
-	std::cin >> R;
-	if (numer.x*numer.x +numer. y*numer.y <= R*R)
-	{
-		std::cout << "Lay in a circle"<<std::endl;
-	}
-	Point start, stop,middle;
-	std::cin >> start.x >> start.y;
-	std::cin >> stop.x >> stop.y;
-	middle.x = (stop.x - start.x)/2;
-	middle.y = (stop.y - start.y)/2;
-	std::cout << middle.x << " " << middle.y << std::endl;
+Point num = { 5,15 };
+Point numer;
+std::cin >> numer.x >> numer.y;
+std::cout << numer.x << " " << numer.y << std::endl;
+int R;
+std::cin >> R;
+if (numer.x*numer.x +numer. y*numer.y <= R*R)
+{
+std::cout << "Lay in a circle"<<std::endl;
+}
+Point start, stop,middle;
+std::cin >> start.x >> start.y;
+std::cin >> stop.x >> stop.y;
+middle.x = (stop.x - start.x)/2;
+middle.y = (stop.y - start.y)/2;
+std::cout << middle.x << " " << middle.y << std::endl;
 }
 */
 
@@ -57,29 +57,29 @@ struct Circle
 };
 /*int main()
 {
-	Circle C1, C2;
-	std::cin >> C1.R >> C1.point.x >> C1.point.y;
-	std::cout << C1.R << C1.point.x << C1.point.y;
-	std::cin >> C2.R >> C2.point.x >> C2.point.y;
-	Point dif;
-	dif.x = abs(C1.point.x - C2.point.x);
-	dif.y = abs(C1.point.y - C2.point.y);
-	int n = 0;
-	if (C1.R + C2.R>dif.x)
-	{
-		n = 1;
-	}
-	if (C1.R + C2.R>dif.y)
-	{
-		n = 1;
-	}
-	switch (n)
-	{
-	case 1:std::cout << "Crossing";
-		break;
-	default:std::cout << "Not crossing";
-		break;
-	}
+Circle C1, C2;
+std::cin >> C1.R >> C1.point.x >> C1.point.y;
+std::cout << C1.R << C1.point.x << C1.point.y;
+std::cin >> C2.R >> C2.point.x >> C2.point.y;
+Point dif;
+dif.x = abs(C1.point.x - C2.point.x);
+dif.y = abs(C1.point.y - C2.point.y);
+int n = 0;
+if (C1.R + C2.R>dif.x)
+{
+n = 1;
+}
+if (C1.R + C2.R>dif.y)
+{
+n = 1;
+}
+switch (n)
+{
+case 1:std::cout << "Crossing";
+break;
+default:std::cout << "Not crossing";
+break;
+}
 }*/
 
 /*Задание №3
@@ -105,27 +105,27 @@ struct Tiangle
 	Color color;
 	int len[3];
 };
-//const char arr[][10] = { "Red","Orange","Yellow","Green","Azure","Blue","Purple"};
+const char ARR[][10] = { "Red","Orange","Yellow","Green","Azure","Blue","Purple"};
 /*int main()
 {
-	Tiangle troa = { Color::Green,{3,4,5} };
-	switch (troa.color)
-	{
-	case 0: std::cout << arr[0];
-		break;
-	case 1: std::cout << arr[1];
-		break;
-	case 2: std::cout << arr[2];
-		break;
-	case 3: std::cout << arr[3];
-		break;
-	case 4: std::cout << arr[4];
-		break;
-	case 5: std::cout << arr[5];
-		break;
-	case 6: std::cout << arr[6];
-		break;
-	}
+Tiangle troa = { Color::Green,{3,4,5} };
+switch (troa.color)
+{
+case 0: std::cout << arr[0];
+break;
+case 1: std::cout << arr[1];
+break;
+case 2: std::cout << arr[2];
+break;
+case 3: std::cout << arr[3];
+break;
+case 4: std::cout << arr[4];
+break;
+case 5: std::cout << arr[5];
+break;
+case 6: std::cout << arr[6];
+break;
+}
 }
 */
 /*Задание №4
@@ -136,34 +136,34 @@ struct Tiangle
 - двух точек, расстояние между которыми минимально.*/
 /*int main()
 {
-	Point arr[20];
-	for (int i = 0; i < 20; ++i)
-	{
-		arr[i].x = ext::GetRandomValue(0, 100);
-		arr[i].y = ext::GetRandomValue(0, 100);
-	}
-	Point min = { 0,0 };
-	for (int i = 0; i < 20; ++i)
-	{
-		if (arr[i].x < min.x)
-		{
-			min.x = arr[i].x;
-			min.y = arr[i].y;
-		}
-	}
-	std::cout << min.x << " " << min.y << std::endl;
-	int len = 0;
-	for (int i = 0; i < 20; ++i)
-	{
-		for (int j = 0; j < 20; ++j)
-		{
-			if (len > sqrt(pow((arr[i].x - arr[j].x), 2)) + pow((arr[i].y - arr[j].y), 2))
-			{
-			len = sqrt(pow((arr[i].x - arr[j].x), 2)) + pow((arr[i].y - arr[j].y), 2);
-			}
-		}
-	}
-	std::cout << len;
+Point arr[20];
+for (int i = 0; i < 20; ++i)
+{
+arr[i].x = ext::GetRandomValue(0, 100);
+arr[i].y = ext::GetRandomValue(0, 100);
+}
+Point min = { 0,0 };
+for (int i = 0; i < 20; ++i)
+{
+if (arr[i].x < min.x)
+{
+min.x = arr[i].x;
+min.y = arr[i].y;
+}
+}
+std::cout << min.x << " " << min.y << std::endl;
+int len = 0;
+for (int i = 0; i < 20; ++i)
+{
+for (int j = 0; j < 20; ++j)
+{
+if (len > sqrt(pow((arr[i].x - arr[j].x), 2)) + pow((arr[i].y - arr[j].y), 2))
+{
+len = sqrt(pow((arr[i].x - arr[j].x), 2)) + pow((arr[i].y - arr[j].y), 2);
+}
+}
+}
+std::cout << len;
 }*/
 /*Задание №5
 Создать массив из 100 элементов типа Triangle.
@@ -174,19 +174,88 @@ struct Tiangle
 - вывести на экран все треугольники, периметр которых превышает значение, введенное с клавиатуры.
 Если подходящих треугольников нет, вывести соответствующее сообщение.
 - количество треугольников каждого цвета*/
-int main()
+/*int main()
 {
 	Tiangle arr[100];
-	int n=0;
+	int n = 0;
 	for (int i = 0; i < 100; ++i)
 	{
-		while (n==0)
+		while (n == 0)
 		{
 			arr[i].len[0] = ext::GetRandomValue(1, 10);
+			arr[i].len[1] = ext::GetRandomValue(1, 10);
+			arr[i].len[2] = ext::GetRandomValue(1, 10);
+			if ((arr[i].len[0] + arr[i].len[1] > arr[i].len[2])
+				&& (arr[i].len[0] < arr[i].len[1] + arr[i].len[2])
+				&&(arr[i].len[0]+ arr[i].len[2]>arr[i].len[1]))
+			{
+				n = 1;
+			}
 		}
-		
+		int temp;
+		temp = ext::GetRandomValue(1, 7);
+		switch (temp)
+		{
+			case 1:	arr[i].color = Color::Red;
+					break;
+			case 2:	arr[i].color = Color::Orange;
+				break;
+			case 3:	arr[i].color = Color::Yellow;
+				break;
+			case 4:	arr[i].color = Color::Green;
+				break;
+			case 5:	arr[i].color = Color::Azure;
+				break;
+			case 6:	arr[i].color = Color::Blue;
+				break;
+			case 7:	arr[i].color = Color::Purple;
+				break;
+
+		}
+		n = 0;
 	}
-}
+	int P;
+	std::cin >> P;
+	for (int i = 0; i < 100; ++i)
+	{
+		if (arr[i].len[0]+ arr[i].len[1] + arr[i].len[2]>P)
+		{
+			std::cout << arr[i].len[0]<<" " << arr[i].len[1]<<" "
+				<< arr[i].len[2] << std::endl;
+			n = 1;
+		}
+	}
+	if (n==0)
+	{
+		std::cout << "No triangles"<<std::endl;
+	}
+	int colr[7]{};
+	for (int i = 0; i < 100; ++i)
+	{
+		++colr[arr[i].color];
+	}
+	for (int i = 0; i < 7; ++i)
+	{
+		switch (i)
+		{
+		case 0: std::cout<<ARR[0];
+			break;
+		case 1: std::cout << ARR[1];
+			break;
+		case 2: std::cout << ARR[2];
+			break;
+		case 3: std::cout << ARR[3];
+			break;
+		case 4: std::cout << ARR[4];
+			break;
+		case 5: std::cout << ARR[5];
+			break;
+		case 6: std::cout << ARR[6];
+			break;
+		}
+		std::cout << " - " << colr[i] << std::endl;
+	}
+}*/
 /*Задание №6
 Написать программу, которая проверяет правильность заданных значений даты.
 Дату хранить с использованием структуры.
@@ -202,3 +271,58 @@ int main()
 29 2 1900					Дата некоррентна
 29 2 2000					Дата корректна
 */
+struct Date
+{
+	int day;
+	int daymax;
+	int month;
+	int year;
+};
+int main()
+{
+	Date date;
+	std::cin >> date.day;
+	std::cin >> date.month;
+	std::cin >> date.year;
+	bool res, leap;
+	int monthn;
+	if ((date.year % 400 == 0) || ((date.year % 4 == 0) && (date.year % 100 != 0)))
+	{
+		leap = true;
+	}
+	else
+	{
+		leap = false;
+	}
+	monthn = date.month % 2;
+	switch(monthn)
+	{
+	case 1:
+		date.daymax = 30;
+			if (date.month == 8)
+			{
+				date.daymax = 31;
+			}
+			break;
+		case 0:
+			date.daymax = 31;
+				if (date.month == 2)
+				{
+					date.daymax = 28;
+				}
+				break;
+	}
+	if ((leap)&&(date.month==2))
+	{
+		date.daymax = 29;
+	}
+	res = (date.day<= date.daymax) && (date.month <= 12);
+	if (res)
+	{
+		std::cout << "true";
+	}
+	else
+	{
+		std::cout << "false";
+	}
+}
